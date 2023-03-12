d2c64accfc1a21aa5a15a5f827b981f4e5abd7ad
collins-self <collinsnater03@gmail.com>
2023-03-12 19:46:42 -0400

    configured eslint

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,10 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import react from '@astrojs/react';
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), image(), tailwind()]
+  integrations: [mdx(), sitemap(), react(), image(), tailwind()]
 });
