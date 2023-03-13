b9f3777e90ec6ecb0f5308f5f70288fd5b392922
collins-self <collinsnater03@gmail.com>
2023-03-13 11:06:21 -0400

    wow initializing again

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Collins.com.';
-export const SITE_DESCRIPTION = 'Blog and portfolio for an ambitious computer science student';
