0cc70223991c6a11fb9466e13be8179b08facf59
collins-self <collinsnater03@gmail.com>
2023-06-27 10:58:15 -0400

    wow

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,11 @@
 
 export const SITE_TITLE = 'collinsself.com';
 export const SITE_DESCRIPTION = 'Professional Portfolio and Blog';
+<<<<<<< Updated upstream
 export const bgColor = `bg-gray-50 dark:bg-stone-800`;
 export const textColor = `text-gray-900 dark:text-gray-200`;
 export const bgAccentColor = 'bg-gray-200 dark:bg-stone-700';
+=======
+export const bgColor = `bg-green-50 dark:bg-slate-600`;
+export const textColor = `text-green-900 dark:text-gray-200`;
+>>>>>>> Stashed changes
